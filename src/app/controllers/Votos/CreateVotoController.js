63779cb0cdc776dca8792fa3798d0d8e5bd81e87
Daniel <danielfilho@mesalvati.com.br>
2022-09-05 16:36:48 -0300

    Correção no controller e service voto

--- src/app/controllers/Votos/CreateVotoController.js
+++ src/app/controllers/Votos/CreateVotoController.js
@@ -1,25 +1,20 @@
-
-import CreateVotoService from '../../services/Votos/CreateVotoService';
+import CreateVotoService from "../../services/Votos/CreateVotoService";
 
 export default class CreateVotoController {
-    
-        constructor() {
-            this.service = new CreateVotoService();
-        }
+  constructor() {
+    this.service = new CreateVotoService();
+  }
 
-    async create (req, res){
-        const {
-            idVoto,
-            idRegVoto,
-            idQuantVotos
-        } = req.body;
-    
-        const createdVoto = await this.service.create(
-            idVoto,
-            idRegVoto,
-            idQuantVotos
-        );
-    
-        return res.status(200).json(createdVoto);
-    }
+  async create(req, res) {
+    const { idVoto, id_registro_voto_eleitor, id_quant_votos_candidato } =
+      req.body;
+
+    const createdVoto = await this.service.create(
+      idVoto,
+      id_registro_voto_eleitor,
+      id_quant_votos_candidato
+    );
+
+    return res.status(200).json(createdVoto);
+  }
 }
