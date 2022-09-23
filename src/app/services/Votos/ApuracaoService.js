2d92db978c811987b0dd11175b32d1443561aec2
kcildo <acaciomonteiro@id.uff.br>
2022-09-22 22:03:47 -0300

    modificando retorno de apuração, incluindo percentual nos votos brancos e retirando o id de voto do retorno

--- src/app/services/Votos/ApuracaoService.js
+++ src/app/services/Votos/ApuracaoService.js
@@ -11,13 +11,13 @@ export default class ApuracaoService {
 
     async index() {
         try{
-            const apuracao = 'select nome_candidato, numero_candidato, id_candidato_voto, count(id_candidato_voto) as total_votos, round((count(*) / (select count(*) from quant_votos_candidatos)*100),2) as percentual from candidatos inner join quant_votos_candidatos on idcandidato = id_candidato_voto group by numero_candidato order by total_votos desc;';
+            const apuracao = 'select nome_candidato, numero_candidato, count(id_candidato_voto) as total_votos, round((count(*) / (select count(*) from quant_votos_candidatos)*100),2) as percentual from candidatos inner join quant_votos_candidatos on idcandidato = id_candidato_voto group by numero_candidato order by total_votos desc;';
 
             const get = await sequelize.query({
                 query:apuracao                
             },{type: QueryTypes.SELECT});
 
-            const apuracaoBranco = 'select *, count(*) as quant_votos_brancos from quant_votos_candidatos where branco_nulo is not null;';
+            const apuracaoBranco = 'select branco_nulo as Branco_ou_Nulo, count(*) as Total, round((count(*) / (select count(*) from quant_votos_candidatos)*100),2) as percentual from quant_votos_candidatos where branco_nulo is not null;';
 
             const getBranco = await sequelize.query({
                 query:apuracaoBranco                
