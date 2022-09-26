33e204539ea0df3ad350d9aa5eb75b12b0d14114
rmls <rodrigomoreiralima@hotmail.com>
2022-09-26 20:00:17 -0300

    correcao na rota de sessao

--- src/routes/routesSession.js
+++ src/routes/routesSession.js
@@ -3,9 +3,11 @@ import Router from 'express';
 //importando controllers para usuários
 import SessionController from '../app/controllers/auth/SessionController';
 
+const sessionController = new SessionController()
+
 const routesSession = new Router();
 
 // Rota para sessão
-routesSession.post('/login', (req,res) => SessionController.create(req, res));
+routesSession.post('/login', (req,res) => sessionController.create(req, res));
 
 export default routesSession;
